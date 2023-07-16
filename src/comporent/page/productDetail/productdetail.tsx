09b5b7973da5340026684562095cd493216a5d22
giangnhph23819 <giangnhph23819@fpt.edu.vn>
2023-07-16 16:53:30 +0700

    feat:creat-Home refs:F2-9

---
+++ src/comporent/page/productDetail/productdetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+const productdetail = ( props: Props ) =>
+{
+    return (
+        <div>productdetail</div>
+    )
+}
+
+export default productdetail
