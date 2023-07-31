9e00a7db5d21c532dbf8e43a7bdfb8b412b843db
giangnhph23819 <giangnhph23819@fpt.edu.vn>
2023-07-31 22:28:46 +0700

    feat:ui search refs:F2-30

--- src/slice/product.tsx
+++ src/slice/product.tsx
@@ -1,33 +1,59 @@
+// productSlice.js
 import { createSlice } from "@reduxjs/toolkit";
 import { Iproduct } from "../interface/product";
 import { fetch, get, remove } from "../action/product";
+
 const initialState = {
-    products: [] as Iproduct[]
-}
-const sliceProduct = createSlice(
-    {
-        name: "product",
-        initialState,
-        reducers: {},
-        extraReducers ( builder )
+    products: [] as Iproduct[],
+    searchKeyword: "", // Từ khóa tìm kiếm
+    searchResults: [] as Iproduct[], // Kết quả tìm kiếm
+};
+
+const sliceProduct = createSlice( {
+    name: "product",
+    initialState,
+    reducers: {
+        addProduct: ( state, action ) =>
         {
-            builder.addCase( fetch.fulfilled, ( state, action ) =>
-            {
-                state.products = action.payload
-            } )
-            builder.addCase( get.fulfilled, ( state, action ) =>
-            {
-
-
-            } )
-            builder.addCase( remove.fulfilled, ( state, action ) =>
-            {
-                const id = action.payload
-                state.products = state.products.filter( ( item: any ) => item.id != id )
-            } )
-
+            // Thêm sản phẩm mới vào danh sách sản phẩm
+            state.products.push( action.payload );
         },
+        updateProduct: ( state, action ) =>
+        {
+            const updatedProduct = action.payload;
+            // Tìm và cập nhật sản phẩm trong danh sách sản phẩm dựa trên id
+            state.products = state.products.map( ( product ) =>
+                product._id === updatedProduct.id ? updatedProduct : product
+            );
+        },
+        setSearchKeyword: ( state, action ) =>
+        {
+            state.searchKeyword = action.payload;
+        },
+        setSearchResults: ( state, action ) =>
+        {
+            state.searchResults = action.payload;
+        },
+    },
+    extraReducers ( builder )
+    {
+        builder.addCase( fetch.fulfilled, ( state, action ) =>
+        {
+            state.products = action.payload;
+        } );
+        builder.addCase( get.fulfilled, ( state, action ) =>
+        {
+            // Cập nhật thông tin sản phẩm trong trạng thái dựa trên action.payload (nếu cần)
+        } );
+        builder.addCase( remove.fulfilled, ( state, action ) =>
+        {
+            const id = action.payload;
+            // Xóa sản phẩm khỏi danh sách sản phẩm dựa trên id
+            state.products = state.products.filter( ( item: any ) => item.id !== id );
+        } );
+    },
+} );
 
-    }
-)
-export const productReducer = sliceProduct.reducer
+export const { addProduct, updateProduct, setSearchKeyword, setSearchResults } =
+    sliceProduct.actions;
+export const productReducer = sliceProduct.reducer;
